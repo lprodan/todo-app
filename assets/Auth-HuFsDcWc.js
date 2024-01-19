b8f92b3f61ffea32fbd1c63ef19ae9ba15ad033c
Vadim Prodan <vprodan@pm.me>
2024-01-19 22:35:57 +0100

    Updates

--- assets/Auth-HuFsDcWc.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-mWNOA-Lg.js";function u(){return t.jsx(r,{})}export{u as default};
---
+++ assets/Auth-Uv0Bupjp.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-r8o4uK72.js";function u(){return t.jsx(r,{})}export{u as default};
